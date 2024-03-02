40c7a31b4e4ee8a1103c9d3d977320788fb1b43c
Andrew Campbell <144943158+CampAndy1995@users.noreply.github.com>
2024-03-01 20:06:11 -0800

    Add files via upload

---
+++ Class_Assignment.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Assignment 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double total;
+
+            total = Multiply(2, 3);
+
+            Console.WriteLine(total);
+            Console.ReadKey();
+
+            //Messages.Hello();
+            //Messages.Waiting();
+            //Messages.Bye();
+
+            //Console.ReadKey();
+        }
+    }
+
+    static double Multiply(double a, double b)
+    {
+        return a * b;
+    }
+
+
+    //class Messages
+    //{
+    //  public static void Hello()
+    //    {
+    //        Console.WriteLine("Hello! Welcome to the program!");
+    //    }
+    //   public static void Waiting()
+    //    {
+    //        Console.WriteLine("Is this the waiting line for the concert?");
+    //    }
+    //   public static void Bye()
+    //    {
+    //        Console.WriteLine("Bye! Come again!!");
+    //    }
+    //}
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Messages.Hello();
+
+            Console.ReadKey();
+        }
+    }
+    class Messages
+    {
+      public static void Hello()
+        {
+            Console.WriteLine("Hello! Welcome to the program!");
+        }
+       public static void Waiting()
+        {
+            Console.WriteLine("Is this the waiting line for the concert?");
+        }
+       public static void Bye()
+        {
+            Console.WriteLine("Bye! Come again!!");
+        }
+    }
+}
